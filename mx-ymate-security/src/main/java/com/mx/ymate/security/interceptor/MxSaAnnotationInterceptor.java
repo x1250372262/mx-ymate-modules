9735f0b4472cbb1d11dceab2c7bbfc697dba4ffe
xujianpeng <1250372262@qq.com>
2023-02-22 11:13:17 +0800

    feat:增加排除路径功能 可以排除哪些路径不执行权限验证

--- mx-ymate-security/src/main/java/com/mx/ymate/security/interceptor/MxSaAnnotationInterceptor.java
+++ mx-ymate-security/src/main/java/com/mx/ymate/security/interceptor/MxSaAnnotationInterceptor.java
@@ -4,6 +4,7 @@ import cn.dev33.satoken.exception.NotLoginException;
 import cn.dev33.satoken.exception.NotPermissionException;
 import cn.dev33.satoken.exception.NotRoleException;
 import cn.dev33.satoken.strategy.SaStrategy;
+import cn.hutool.core.text.AntPathMatcher;
 import com.mx.ymate.dev.code.Code;
 import com.mx.ymate.dev.constants.Constants;
 import com.mx.ymate.dev.result.MxResult;
@@ -16,10 +17,11 @@ import com.mx.ymate.security.base.model.SecurityUser;
 import com.mx.ymate.security.dao.ISecurityUserDao;
 import com.mx.ymate.security.exception.MxLoginException;
 import net.ymate.platform.core.YMP;
-import net.ymate.platform.core.beans.annotation.Inject;
 import net.ymate.platform.core.beans.intercept.AbstractInterceptor;
 import net.ymate.platform.core.beans.intercept.InterceptContext;
 import net.ymate.platform.core.beans.intercept.InterceptException;
+import net.ymate.platform.webmvc.context.WebContext;
+import org.apache.commons.lang3.StringUtils;
 
 import java.lang.reflect.Method;
 import java.util.Objects;
@@ -42,8 +44,40 @@ public class MxSaAnnotationInterceptor extends AbstractInterceptor {
     public MxSaAnnotationInterceptor() {
     }
 
+    /**
+     * 检查路径 返回true代表不验证
+     *
+     * @param requestUri
+     * @return
+     */
+    private boolean checkPath(String requestUri) {
+        if (requestUri.endsWith("/")) {
+            requestUri = requestUri.substring(0, requestUri.length() - 1);
+        }
+        String excludePathPatterns = securityConfig.excludePathPatterns();
+        if (StringUtils.isBlank(excludePathPatterns)) {
+            return true;
+        }
+        String[] excludePathPatternArray = excludePathPatterns.split("\\|");
+        if (excludePathPatternArray.length <= 0) {
+            return true;
+        }
+        boolean flag = false;
+        for (String excludePathPattern : excludePathPatternArray) {
+            flag = new AntPathMatcher().match(excludePathPattern, requestUri);
+            if (flag) {
+                break;
+            }
+        }
+        return flag;
+    }
+
     @Override
     protected Object before(InterceptContext context) throws InterceptException {
+        String requestUri = WebContext.getRequest().getRequestURI();
+        if (checkPath(requestUri)) {
+            return null;
+        }
         Method method = context.getTargetMethod();
         // 进行验证
         try {
@@ -70,7 +104,7 @@ public class MxSaAnnotationInterceptor extends AbstractInterceptor {
             return MxResult.create(Code.NOT_LOGIN).toMxJsonView();
         } catch (MxLoginException mxLoginException) {
             return MxResult.create(Code.NOT_LOGIN.code()).msg(mxLoginException.getMessage()).toMxJsonView();
-        }catch (NotPermissionException | NotRoleException notPermissionException) {
+        } catch (NotPermissionException | NotRoleException notPermissionException) {
             return MxResult.create(Code.NOT_PERMISSION).toMxJsonView();
         } catch (Exception e) {
             throw new RuntimeException(e);
