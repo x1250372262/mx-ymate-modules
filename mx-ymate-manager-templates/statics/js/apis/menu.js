8b423c9d233f78e8a40e50c338fe1bbddaa7f245
xujianpeng <1250372262@qq.com>
2023-09-06 11:49:52 +0800

    feat: 菜单权限

--- mx-ymate-manager-templates/statics/js/apis/menu.js
+++ mx-ymate-manager-templates/statics/js/apis/menu.js
@@ -8,9 +8,3 @@ MENU_UPDATE = baseUrl + "/mx/security/menu/update";
 MENU_DETAIL = baseUrl + "/mx/security/menu/detail";
 //菜单删除
 MENU_DELETE = baseUrl + "/mx/security/menu/delete";
-//菜单角色列表
-MENU_ROLE_LIST = baseUrl + "/mx/security/menu/role/list";
-//菜单添加角色
-MENU_ROLE_CREATE = baseUrl + "/mx/security/menu/role/create";
-//菜单删除角色
-MENU_ROLE_DELETE = baseUrl + "/mx/security/menu/role/delete";
